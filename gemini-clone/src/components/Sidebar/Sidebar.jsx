7e9e43588b14ba43f35f38be71e673bd95d7b356
Laura Vanessa Zuluaga <49324952+LauraVZuluaga@users.noreply.github.com>
2024-05-28 17:40:44 -0500

    First structure

---
+++ gemini-clone/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <div className='text-3xl font-bold underline'>
+      holi
+    </div>
+  )
+}
+
+export default Sidebar
